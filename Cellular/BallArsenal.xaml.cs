dded9507045991167431ec3ce2bc957379c9c810
HalfJw <zcox@ycp.edu>
2025-02-15 17:21:17 -0500

    Added Ballspinner and App Run
    I added the ballspinner page from the last group but it has no functionality. I also got the app working on the virtual phone.

---
+++ Cellular/BallArsenal.xaml.cs
@@ -0,0 +1,17 @@
+namespace Cellular;
+
+public partial class BallArsenal : ContentPage
+{
+    public BallArsenal()
+    {
+        InitializeComponent();
+    }
+
+    async private void OnAddBallBtnClicked(object sender, EventArgs e)
+    {
+        //Navigation.PushAsync(new NewBall());
+    }
+
+
+
+}
---
+++ Cellular/ViewModel/User.cs
@@ -0,0 +1,13 @@
+using SQLite;
+namespace Cellular.ViewModel;
+
+[Table("user")]
+public class User
+{
+    [PrimaryKey, AutoIncrement, Column("UserId")]
+    public int UserId { get; set; }
+    public  string? UserName { get; set; } // ? means nullable and I did it so it would work with SQLite, We might want to change this
+    public  string? Password { get; set; } // ? means nullable and I did it so it would work with SQLite, We might want to change this
+    public DateTime LastLogin { get; set; } 
+    public string? BallList { get; set; }
+}
