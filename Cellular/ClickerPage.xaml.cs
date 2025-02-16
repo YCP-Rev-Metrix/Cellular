6b95057a6474c5b6d6186eae35af8ef47e53aa0b
Josh Byers <57502749+JByers1209@users.noreply.github.com>
2025-02-16 13:30:44 -0500

    Made Basic home page. Buttons don't work yet since there arent other pages to link to.

---
+++ Cellular/ClickerPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace Cellular
+{
+    public partial class ClickerPage : ContentPage
+    {
+        int count = 0;
+
+        public ClickerPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+
+}
