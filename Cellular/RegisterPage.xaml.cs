56a76fd07553959342fcf94c11f06d194ff3250d
cmack6 <cmack6@ycp.edu>
2025-02-20 17:25:35 -0500

    registration page created
    currently it doesnt create anything it just signs you in and links you to the home page

---
+++ Cellular/RegisterPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Maui.Storage;
+
+namespace Cellular
+{
+    public partial class RegisterPage : ContentPage
+    {
+        public RegisterPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnRegisterClicked(object sender, EventArgs e)
+        {
+            string username = entryUsername.Text;
+            string password = entryPassword.Text;
+            string cpassword = entryConfirmPassword.Text;
+
+            // checks for username existing
+            if (username == "string")
+            {
+                DisplayAlert("Registration Error", "User already exists", "OK");
+            }
+            else if (password.Equals(cpassword))
+            {
+                DisplayAlert("Registration Error", "Passwords do not match", "OK");
+            }
+            else
+            {
+                Preferences.Set("IsLoggedIn", true);
+
+                // Update menu and navigate to Home
+                ((AppShell)Shell.Current).UpdateMenuForLoginStatus(true);
+                Shell.Current.GoToAsync("//MainPage");
+            }
+        }
+    }
+}
+
