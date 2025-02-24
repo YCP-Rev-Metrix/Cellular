0cb94bbd150842dda134f7e64462b88e4fec8db0
cmack6 <cmack6@ycp.edu>
2025-02-24 16:36:29 -0500

    account page made, sign out button made, arsenal misspell fixed
    sign out button should be removed when not signed in. otherwise, the commit name says it all

---
+++ Cellular/Account.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Maui.Storage;
+
+namespace Cellular
+{
+    public partial class AccountPage : ContentPage
+    {
+        public AccountPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnSignoutClicked(object sender, EventArgs e)
+        {
+             Preferences.Set("IsLoggedIn", false);
+
+             // Update menu and navigate to Home after logging out
+             ((AppShell)Shell.Current).UpdateMenuForLoginStatus(false);
+             Shell.Current.GoToAsync("//MainPage");
+        }
+
+        private void OnStatsClicked(object sender, EventArgs e)
+        {
+            // Implement when stats are figured out
+        }
+    }
+}
+
--- Cellular/AppShell.xaml.cs
+++ Cellular/AppShell.xaml.cs
@@ -15,19 +15,35 @@ namespace Cellular
 
         public void UpdateMenuForLoginStatus(bool isLoggedIn)
         {
+            //ShellItem footer = Items.Last();
             Items.Clear(); // Clear existing items
+            //Items.Add(footer);
 
             if (!isLoggedIn)
             {
                 Items.Add(new ShellContent { Content = new MainPage(), Title = "Home", Route = "MainPage" });
                 Items.Add(new ShellContent { Content = new LoginPage(), Title = "Login", Route = "login" });
+
             }
             else
             {
                 Items.Add(new ShellContent { Content = new MainPage(), Title = "Home", Route = "MainPage" });
                 Items.Add(new ShellContent { Content = new BallArsenal(), Title = "Ball Arsenal", Route = "BallArsenal" });
+                Items.Add(new ShellContent { Content = new AccountPage(), Title = "Account", Route = "AccountPage" });
                 Items.Add(new ShellContent { Content = new ClickerPage(), Title = "Clicker", Route = "ClickerPage" });
+                
+
             }
+        }
+
+        public void OnSignoutClicked(object sender, EventArgs e)
+        {
+            Preferences.Set("IsLoggedIn", false);
+
+            // Update menu and navigate to Home after logging out
+            ((AppShell)Shell.Current).UpdateMenuForLoginStatus(false);
+            Shell.Current.GoToAsync("//MainPage");
+
         }
     }
 }
