3670e93fef93ba02044b2a056b2bc834145d8f52
Josh Byers <57502749+JByers1209@users.noreply.github.com>
2025-03-30 23:55:44 -0400

    Create FrameRepository.cs

---
+++ Cellular/Data/FrameRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SQLite;
+using Cellular.ViewModel;
+
+namespace Cellular.Data
+{
+    public class FrameRepository
+    {
+        private readonly SQLiteAsyncConnection _conn;
+
+        // Constructor ensures the connection is initialized
+        public FrameRepository(SQLiteAsyncConnection conn)
+        {
+            _conn = conn ?? throw new ArgumentNullException(nameof(conn));
+        }
+
+        // Initialize the database table
+        public async Task InitAsync()
+        {
+            await _conn.CreateTableAsync<BowlingFrame>();
+        }
+
+        // Adds a frame to the database
+        public async Task AddFrame(BowlingFrame frame)
+        {
+            await _conn.InsertAsync(frame);
+        }
+
+        // Updates a frame
+        public async Task UpdateFrameAsync(BowlingFrame frame)
+        {
+            await _conn.UpdateAsync(frame);
+        }
+    }
+}
