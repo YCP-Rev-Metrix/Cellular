bf8287a16b637715626fa76cfed4e66b7d2489ee
jbyers1209 <josmbyers@gmail.com>
2026-03-30 12:40:55 -0400

    Updated Event and Establishment class
    I added extra variables to events and establishments.

--- Cellular/ViewModel/Establishment.cs
+++ Cellular/ViewModel/Establishment.cs
@@ -1,4 +1,5 @@
 ﻿using SQLite;
+using System.ComponentModel.DataAnnotations;
 namespace Cellular.ViewModel;
 
 
@@ -7,11 +8,20 @@ public class Establishment
 {
     [PrimaryKey, AutoIncrement]
     public int EstaID { get; set; }
+    [Required]
     public int UserId { get; set; }
-    public string Name { get; set; }
+    [Required]
+    public string FullName { get; set; }
+    [Required, Unique]
+    public string NickName { get; set; }
+    public string GPSLocation { get; set; }
+    public bool HomeHouse { get; set; }
+    [Required]
+    public string Reason { get; set; }
+    public string Address { get; set; }
+    public string PhoneNumber { get; set; }
     public string Lanes { get; set; }
     public string Type { get; set; }
-    public string Location { get; set; }
 
-
+    public bool Enabled { get; set; }
 }
--- Cellular/ViewModel/Event.cs
+++ Cellular/ViewModel/Event.cs
@@ -1,4 +1,5 @@
 using SQLite;
+using System.ComponentModel.DataAnnotations;
 
 namespace Cellular.ViewModel
 {
@@ -7,12 +8,24 @@ namespace Cellular.ViewModel
     {
         [PrimaryKey, AutoIncrement]
         public int EventId { get; set; }
+        [Required]
         public int UserId { get; set; }
-        public string? Name { get; set; }
+        [Required, Unique]
+        public string? LongName { get; set; }
+        [Required, Unique]
+        public string? NickName { get; set; }
         public string? Type { get; set; }
         public string? Location { get; set; }
+        public DateOnly? StartDate { get; set; }
+        public DateOnly? EndDate { get; set; }
+        public string? WeekDay { get; set; }
+        public TimeOnly? StartTime { get; set; }
+        public int NumGamesPerSession { get; set; }
         public int? Average { get; set; }
+        public string? Schedule { get; set; }
         public int? Stats { get; set; }
         public string? Standings { get; set; }
+
+        public bool Enabled { get; set; }
     }
 }
